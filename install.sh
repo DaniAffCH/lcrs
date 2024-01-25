c180e6b0b4b2e02c14b5e964797a05140aabd7b1
DaniAffCH <danieleaffinita2000@gmail.com>
2024-01-25 20:00:50 +0100

    installation

--- install.sh
+++ install.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-pip install -e .
-cd ../calvin_env/tacto
-pip install -e .
-cd ..
-pip install -e .
+venv_name="lcrsVenv"
+
+if command -v python3 &> /dev/null; then
+    if [ ! -d "$venv_name" ]; then
+        python3 -m venv "$venv_name"
+    fi
+
+    source "$venv_name/bin/activate"
+    pip install -e .
+    cd calvin
+    sudo chmod +x ./install.sh
+    ./install.sh
+else
+    echo "Error: Python3 is not installed. Please install Python3 before running this script."
+    exit 1
+fi
+
