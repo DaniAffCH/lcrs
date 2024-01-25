0abe8190191a7732bf299ac976991b8876fc6960
DaniAffCH <danieleaffinita2000@gmail.com>
2024-01-25 20:03:38 +0100

    removed root

--- install.sh
+++ install.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-venv_name="lcrsVenv"
-
-if command -v python3 &> /dev/null; then
-    if [ ! -d "$venv_name" ]; then
-        python3 -m venv "$venv_name"
-    fi
-
-    source "$venv_name/bin/activate"
-    pip install -e .
-    cd calvin
-    sudo chmod +x ./install.sh
-    ./install.sh
-else
-    echo "Error: Python3 is not installed. Please install Python3 before running this script."
-    exit 1
-fi
-
+pip install -e .
+cd calvin
+./install.sh
